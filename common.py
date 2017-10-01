5dbe1699885b0229d28368542df8f327add5b4bf
toni <tkukurin@gmail.com>
2017-10-01 21:59:53 +0100

    lab03 Newton-Rhapson and gradient descent. extracted part of functionality to common

--- common.py
+++ common.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+# https://stackoverflow.com/questions/1301735/counting-python-method-calls-within-another-method
+def counted(fn):
+    def wrapper(*args, **kwargs):
+        wrapper.called += 1
+        return fn(*args, **kwargs)
+    wrapper.called = 0
+    wrapper.__name__= fn.__name__
+    return wrapper
+
+def counts(fn):
+    def wrapper(f, *args, **kwargs):
+        f.called = 0
+        try:
+            return fn(f, *args, **kwargs)
+        finally:
+            wrapper.calls_to_function = f.called
+    wrapper.__name__= fn.__name__
+    return wrapper
+
 def compose(*functions):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
@@ -11,3 +30,6 @@ def swap(lst, index1, index2):
     lst[index1] = lst[index2]
     lst[index2] = tmp
     return lst
+
+pt = lambda *x: np.array(x, dtype=float)
+dim = lambda pt: len(pt)
